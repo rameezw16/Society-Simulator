81e229fef12a5957249a0343e8b02357d60c63e5
Your Name <you@example.com>
2023-11-01 20:26:57 +0500

    Spaghetti code, will refactor tomorrow

--- predator_prey.cpp
+++ predator_prey.cpp
@@ -3,88 +3,98 @@
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 #include <iostream>
-#include <string>
 #include <math.h>
 #include <random>
+#include <string>
 #include <vector>
-
-
 
 class Charecter {
 
-public: //are public just for testing at the moment
-	int health = 100;
-	int radius;
-	bool predator;
-	bool prey;
+public: // are public just for testing at the moment
+  int health = 100;
+  int radius;
+  bool predator;
+  bool prey;
 
-	SDL_Renderer * r;
-	SDL_Rect rect;
+  SDL_Renderer *r;
+  SDL_Rect rect;
 
-	Charecter(SDL_Renderer * r, int x, int y, int w, int h, int radius) : r(r), radius(radius) {
-		this->rect.x = x;
-		this->rect.y = y;
-		this->rect.w = w;
-		this->rect.h = h;
-	};
+  Charecter *target = this;
 
-	void display() {
-		if (this->health > 0) SDL_SetRenderDrawColor(r, 0xff, 0x00, 0x00, 0xff);
-		else SDL_SetRenderDrawColor(r, 0x00, 0x00, 0xff, 0xff);
-			SDL_RenderFillRect(r,&rect);
-			SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
-	};
+  Charecter(SDL_Renderer *r, int x, int y, int w, int h, int radius)
+      : r(r), radius(radius) {
+    this->rect.x = x;
+    this->rect.y = y;
+    this->rect.w = w;
+    this->rect.h = h;
+  };
 
-	bool isInRange(Charecter &other) { //checks whether the other is in range of this character
+  void display() {
+    if (this->health > 0)
+      SDL_SetRenderDrawColor(r, 0xff, 0x00, 0x00, 0xff);
+    else
+      SDL_SetRenderDrawColor(r, 0x00, 0x00, 0xff, 0xff);
+    SDL_RenderFillRect(r, &rect);
+    SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
+  };
 
-		int d1 = this->rect.x - other.rect.x;
-		int d2 = this->rect.y - other.rect.y;
+  bool isInRange(Charecter &other) { // checks whether the other is in range of
+                                     // this character
 
+    int d1 = this->rect.x - other.rect.x;
+    int d2 = this->rect.y - other.rect.y;
 
-		if (std::sqrt(d1 * d1 + d2 * d2) < this->radius) {
-			std::cout << std::sqrt(d1 * d1 + d2 * d2);
-			return true;
-		};
-		return false;
-	};
+    if (std::sqrt(d1 * d1 + d2 * d2) < this->radius) {
+      return true;
+    };
+    return false;
+  };
 
-	void moveInDirection(Charecter &other) {
-		int factor = 20;
-		SDL_Point temp {(other.rect.x - this->rect.x)/factor, (other.rect.y - this->rect.x)/factor} ; //turn this into float tuple
-		this->rect.x += temp.x;
-		this->rect.y += temp.y;
-	};
+  void moveToTarget() {
+    int factor = 20;
+		if (this->target == nullptr) return;
+    SDL_Point temp{(this->target->rect.x - this->rect.x) / factor,
+                   (this->target->rect.y - this->rect.x) / factor}; // turn this into float tuple
+    if (this->rect.x + temp.x < 400 and this->rect.y + temp.y < 400 and
+        this->rect.x - temp.x > 0 and this->rect.y - temp.y > 0) {
 
-	void randomWalk() {
+				this->rect.x += temp.x;
+				this->rect.y += temp.y;
+		}
+  };
 
-		int choice = rand() % 3;
-		int s = 5;
-		switch (choice) {
-		case 0 :
-			this->rect.x += s;
-			this->rect.y += 0;
-			break;
-		case 1:
-			this->rect.x -= s;
-			this->rect.y += 0;
-			break;
-		case 2:
-			this->rect.x += 0;
-			this->rect.y += s;
-			break;
-		case 3:
-			this->rect.x += 0;
-			this->rect.y += s;
-			break;
-		};
-	};
+  void randomWalk() {
+
+    int choice = rand() % 3;
+    int s = 5;
+    if (this->rect.x + s < 400 and this->rect.y + s < 400 and
+        this->rect.x - s > 0 and this->rect.y - s > 0) {
+      switch (choice) {
+      case 0:
+        this->rect.x += s;
+        this->rect.y += 0;
+        break;
+      case 1:
+        this->rect.x -= s;
+        this->rect.y += 0;
+        break;
+      case 2:
+        this->rect.x += 0;
+        this->rect.y += s;
+        break;
+      case 3:
+        this->rect.x += 0;
+        this->rect.y += s;
+        break;
+      };
+    };
+  };
 };
-
 
 int main() {
   const int height = 400;
   const int width = 400;
-SDL_Window *w;
+  SDL_Window *w;
 
   SDL_Point window_position = {SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED};
 
@@ -100,56 +110,59 @@ SDL_Window *w;
 
   SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
 
-	std::vector<Charecter> predators;
-		int xpos;
-		int ypos;
-		int pWidth;
-		int pHeight;
-		int radius;
-		int num = 5; 
+  std::vector<Charecter *> predators;
+  int xpos;
+  int ypos;
+  int pWidth;
+  int pHeight;
+  int radius;
+  int num = 5;
 
-	for(int i = 0; i < num; i++) {
-		xpos = rand() % width;
-		ypos = rand() % height;
-		pWidth = rand() % 40;
-		pHeight = pWidth;
-		Charecter temp{r, xpos,ypos,pWidth,pHeight,radius};
-		predators.push_back(temp);
-	};
+  for (int i = 0; i < num; i++) {
+    xpos = width - (rand() % width);
+    ypos = rand() % height;
+    pWidth = rand() % 40;
+    pHeight = pWidth;
+    Charecter *temp = new Charecter{r, xpos, ypos, pWidth, pHeight, radius};
+    predators.push_back(temp);
+  };
 
-	bool quit = false;
+  bool quit = false;
   SDL_Event e;
   while (!quit) {
     while (SDL_PollEvent(&e) > 0) {
 
-			switch(e.type) {
-												case SDL_QUIT:
-													quit = true;
-                          break;
-			};
-		};
+      switch (e.type) {
+      case SDL_QUIT:
+        quit = true;
+        break;
+      };
+    };
 
-      SDL_RenderClear(r);
+    SDL_RenderClear(r);
 
-			for (int i = 0; i < num; i++) {
-				predators[i].display();
-			};
+    for (int i = 0; i < num; i++) {
+      predators[i]->display();
+    };
 
-			for (int i = 0; i < num; i++) {
-				predators[i].randomWalk();
-			};
+    for (int i = 0; i < num; i++) {
+      predators[i]->randomWalk();
+    };
 
-			for (int i = 0; i < num; i++) {
-				for (int j = 0; j < num; j++) {
-					if (predators[i].isInRange(predators[j])) {
-						predators[i].moveInDirection(predators[j]);
-						predators[j].health -=100;
-					}
-				}
-			};
+    for (int i = 0; i < num; i++) {
+      for (int j = 0; j < num; j++) {
+        if (predators[i]->isInRange(*predators[j])) {
+          predators[i]->moveToTarget();
+					if (predators[i]->target != nullptr) predators[i]->target->health -= 1;
+        }
+      }
+    };
 
+    SDL_RenderPresent(r);
+    SDL_Delay(100);
+  };
 
-      SDL_RenderPresent(r);
-			SDL_Delay(10);
+  for (int i = 0; i < num; i++) {
+    delete predators[i];
   };
 };
