b455e135ab74ec6e81edb6366b9540e38e23f4aa
Your Name <you@example.com>
2023-11-01 19:47:59 +0500

    predator tracks prey, and decreases its health

--- predator_prey.cpp
+++ predator_prey.cpp
@@ -27,7 +27,8 @@ public: //are public just for testing at the moment
 	};
 
 	void display() {
-			SDL_SetRenderDrawColor(r, 0xff, 0x00, 0x00, 0xff);
+		if (this->health > 0) SDL_SetRenderDrawColor(r, 0xff, 0x00, 0x00, 0xff);
+		else SDL_SetRenderDrawColor(r, 0x00, 0x00, 0xff, 0xff);
 			SDL_RenderFillRect(r,&rect);
 			SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
 	};
@@ -39,9 +40,17 @@ public: //are public just for testing at the moment
 
 
 		if (std::sqrt(d1 * d1 + d2 * d2) < this->radius) {
+			std::cout << std::sqrt(d1 * d1 + d2 * d2);
 			return true;
 		};
 		return false;
+	};
+
+	void moveInDirection(Charecter &other) {
+		int factor = 20;
+		SDL_Point temp {(other.rect.x - this->rect.x)/factor, (other.rect.y - this->rect.x)/factor} ; //turn this into float tuple
+		this->rect.x += temp.x;
+		this->rect.y += temp.y;
 	};
 };
 
@@ -49,8 +58,7 @@ public: //are public just for testing at the moment
 int main() {
   const int height = 400;
   const int width = 400;
-
-  SDL_Window *w;
+SDL_Window *w;
 
   SDL_Point window_position = {SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED};
 
@@ -66,8 +74,8 @@ int main() {
 
   SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
 
-	Charecter predator1(r, 100, 100, 10, 10, 30);
-	Charecter predator2(r, 200, 200, 10, 10, 30);
+	Charecter predator1(r, 100, 100, 10, 10, 150);
+	Charecter predator2(r, 200, 200, 10, 10, 150);
 	bool quit = false;
   SDL_Event e;
   while (!quit) {
@@ -84,7 +92,12 @@ int main() {
 			predator1.display();
 			predator2.display();
 
+			if (predator1.isInRange(predator2)) {
+				predator1.moveInDirection(predator2);
+				predator2.health -= 1;
+			};
+
 
       SDL_RenderPresent(r);
   };
-}
+};
