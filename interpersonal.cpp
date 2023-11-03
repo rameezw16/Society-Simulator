7383e90ce9fcb7e8646c8f36a777d9d671d99fca
Musab1Blaser <musab16000@gmail.com>
2023-11-03 22:16:40 +0500

    talking implemented

--- interpersonal.cpp
+++ interpersonal.cpp
@@ -7,6 +7,9 @@
 #include <fstream>
 
 std::mt19937 mt(static_cast<unsigned>(std::time(0)));
+class Entity;
+std::vector<Entity*> Entities;
+
 
 class Entity
 {
@@ -19,9 +22,9 @@ class Entity
         std::string name;
         int health, wealth, happiness;
         int totalRespect, totalProfessionalism, totalLove;
-        std::map<int, std::vector<int>> relationShips; // Respect/Jealousy, Professional/Unprofessionalism, Love/Hate 
+        std::map<int, std::vector<int>> relationShips; // id -> relationship: Respect/Jealousy, Professional/Unprofessionalism, Love/Hate 
 
-        Entity(std::string name, std::vector<Entity>& Entities)
+        Entity(std::string name)
         {
             this->id = count;
             count++;
@@ -33,13 +36,13 @@ class Entity
 
             std::ofstream entityFile;
             entityFile.open(name+".txt");
-            entityFile << this->name << " created with id: " << this->id << " health: " << this->health << " wealth: " << this->wealth << " happiness: " << this->happiness << "\n";
+            entityFile << this->name << " created with id: " << this->id << " health: " << this->health << ", wealth: " << this->wealth << ", happiness: " << this->happiness << "\n";
             entityFile.close();
 
-            for (Entity x : Entities)
+            for (Entity* x : Entities)
             {
-                this->initialise_Relation(&x);
-                x.initialise_Relation(this);
+                this->initialise_Relation(x);
+                x->initialise_Relation(this);
             }
         }
 
@@ -53,15 +56,33 @@ class Entity
             this->totalProfessionalism += professionalism;
             this->totalRespect += respect;
             this->totalLove += love;
-            relationShips[other->id] = {respect, professionalism, love};
+            this->relationShips[other->id] = {respect, professionalism, love};
             std::ofstream entityFile;
             entityFile.open(this->name+".txt", std::ios::app);
-            entityFile << "Towards " << other->name << " has Respect: " << respect << " Professionalism: " << professionalism << " Love: " << love << std::endl;
+            entityFile << "Towards " << other->name << " has Respect: " << respect << ", Professionalism: " << professionalism << ", Love: " << love << std::endl;
+            entityFile.close();
+        }
+
+        void newDay(int x)
+        {
+            std::ofstream entityFile;
+            entityFile.open(this->name+".txt", std::ios::app);
+            entityFile << "Day " << x << std::endl;
+            entityFile.close();
+        }
+
+        ~Entity()
+        {
+            std::ofstream entityFile;
+            entityFile.open(name+".txt", std::ios::app);
+            entityFile << this->name << " (id: " << this->id << ") ended with " << this->id << " health: " << this->health << ", wealth: " << this->wealth << ", happiness: " << this->happiness << "\n";
+            for (auto x : this->relationShips)
+            {
+                entityFile << "For " << Entities[x.first]->name << " ended with Respect: " << x.second[0] << ", Professionalism: " << x.second[1] << ", Love: " << x.second[2] << std::endl;
+            }
             entityFile.close();
         }
 };
-
-std::vector<Entity> Entities;
 
 class Interaction
 {
@@ -95,17 +116,17 @@ class SocialInteraction : public Interaction
         {
             int c = mt() % (p1.totalLove);
             int curLove = 0;
-            int other_id = 0;
+            int other_id = 1;
             for (auto x : p1.relationShips)
             {
                 curLove += x.second[2];
                 other_id = x.first;
                 if (curLove > c) break;
             }
-            Entity& p2 = Entities[other_id];
+            Entity& p2 = *Entities[other_id];
 
-            p1.happiness += (int) ((p1.personalityTraits[0] - 30) * mt() % 4) / 3;
-            int x = (mt() % 7 - 2) ;
+            p1.happiness += (int) ((p1.personalityTraits[0] - 30) * mt() % 4);
+            int x = (mt() % 11 - 4) ;
             p1.relationShips[other_id][2] += x;
             p1.totalLove += x;
 
@@ -113,21 +134,18 @@ class SocialInteraction : public Interaction
             p1File.open(p1.name+".txt", std::ios::app);
             p1File << p1.name << " talked to " << p2.name << std::endl;
             p1File << p1.name << " has happiness: " << p1.happiness << " and loves " << p2.name << " " << x << " units more"   << "\n";
-            
             p1File.close();
 
-            p2.happiness += (int) ((p2.personalityTraits[0] - 30) * mt() % 4) / 3;
-            int y = (mt() % 7 - 2) ;
+            p2.happiness += (int) ((p2.personalityTraits[0] - 30) * mt() % 4);
+            int y = (mt() % 11 - 4) ;
             p2.relationShips[p1.id][2] += y;
             p2.totalLove += x;
 
-
             std::ofstream p2File;
-            p1File.open(p2.name+".txt", std::ios::app);
-            p1File << p1.name << " talked to " << p2.name << std::endl;
-            p1File << p2.name << " has happiness: " << p2.happiness << " and loves " << p1.name << " " << y << " units more"   << "\n";
-            
-
+            p2File.open(p2.name+".txt", std::ios::app);
+            p2File << p1.name << " talked to " << p2.name << std::endl;
+            p2File << p2.name << " has happiness: " << p2.happiness << " and loves " << p1.name << " " << y << " units more"   << "\n";
+            p2File.close();
         }
         void Hurt(Entity& p1)
         {
@@ -145,19 +163,25 @@ int Entity::count = 0;
 int main()
 {
     // for (int i = 0; i < 10; i++) std::cout << mt() << std::endl;
-    Entities.push_back(Entity("Joe", Entities));
-    Entities.push_back(Entity("Roe", Entities));
-    Entities.push_back(Entity("Boe", Entities));
+    Entities.push_back(new Entity("Joe"));
+    Entities.push_back(new Entity("Roe"));
+    Entities.push_back(new Entity("Boe"));
 
     Interaction* act = new SocialInteraction();
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 1; i < 10; i++)
     {
-        for (Entity& x : Entities)
+        for (Entity* x : Entities)
         {
-            act->interact(x);
+            x->newDay(i);
+        }
+        for (Entity* x : Entities)
+        {
+            act->interact(*x);
         }
     }
+
+    for (Entity* x : Entities) delete x;
     // Entities a = Entities("Joe");
     // Entities b = Entities("Roe");
     // Entities c = Entities("Boe");
