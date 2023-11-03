6a28d38e5c96ce82b63dbb9b5c4a9f34bbf9c484
Musab1Blaser <musab16000@gmail.com>
2023-11-03 19:01:58 +0500

    storing entity details in files

--- interpersonal.cpp
+++ interpersonal.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <ctime>
 #include <vector>
+#include <fstream>
 
 std::mt19937 mt(static_cast<unsigned>(std::time(0)));
 
@@ -20,14 +21,17 @@ class Entities
             health = 30 + (mt() % 60);
             wealth = 30 + (mt() % 60);
             happiness = 20 + 0.2*(health*(mt()%4) + wealth*(mt()%4)); // max val is 20 + 0.2*(180 + 180) = 20 + 0.2*360 = 20 + 72 = 92
-            printf("%s created with health: %i wealth: %i happiness: %i\n", this->name.c_str(), this->health, this->wealth, this->happiness);
+            std::ofstream entityFile;
+            entityFile.open(name+".txt");
+            entityFile << this->name << " created with health: " << this->health << " wealth: " << this->wealth << " happiness: " << this->happiness << "\n";
+            entityFile.close();
         }
         
 };
 
 int main()
 {
-    for (int i = 0; i < 10; i++) std::cout << mt() << std::endl;
+    // for (int i = 0; i < 10; i++) std::cout << mt() << std::endl;
     Entities a = Entities("Joe");
     Entities b = Entities("Roe");
     Entities c = Entities("Boe");
