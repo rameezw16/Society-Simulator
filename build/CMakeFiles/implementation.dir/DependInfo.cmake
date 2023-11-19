f5e9f38498f97d39361f11929632a05afd13f865
Musab1Blaser <musab16000@gmail.com>
2023-11-19 16:02:24 +0500

    tmp

---
+++ build/CMakeFiles/implementation.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/musab/Documents/OOP Project/peopleSim/src/entity.cpp" "CMakeFiles/implementation.dir/src/entity.cpp.o" "gcc" "CMakeFiles/implementation.dir/src/entity.cpp.o.d"
+  "/home/musab/Documents/OOP Project/peopleSim/src/main.cpp" "CMakeFiles/implementation.dir/src/main.cpp.o" "gcc" "CMakeFiles/implementation.dir/src/main.cpp.o.d"
+  "/home/musab/Documents/OOP Project/peopleSim/src/perlin.cpp" "CMakeFiles/implementation.dir/src/perlin.cpp.o" "gcc" "CMakeFiles/implementation.dir/src/perlin.cpp.o.d"
+  "/home/musab/Documents/OOP Project/peopleSim/src/spritesheet.cpp" "CMakeFiles/implementation.dir/src/spritesheet.cpp.o" "gcc" "CMakeFiles/implementation.dir/src/spritesheet.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
