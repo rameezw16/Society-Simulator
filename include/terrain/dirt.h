a22dc8a8a603ee5aebfdecaa047929ee0790454e
Hamad <hk08394@st.habib.edu.pk>
2023-11-24 23:15:22 +0500

    Brighter dirt

--- include/terrain/dirt.h
+++ include/terrain/dirt.h
@@ -3,7 +3,9 @@
 
 class Dirt : public Terrain { //making a class so we can extend functionality later
 public:
-	Dirt(const int pos_x, const int pos_y, const int spritesheet_pos_x = 0, const int spritesheet_pos_y = 0);
+	Dirt(const int pos_x, const int pos_y, const int spritesheet_pos_x = 9, const int spritesheet_pos_y = 33);
+	//13 and 24 for bright dirt
+	//9 and 33 for darker dirt
 	~Dirt();
 	void interact_randomly() override;
 };
