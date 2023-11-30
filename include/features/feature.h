85ee61751f7287e2aca069f6b07e1bc76a0da695
Hamad <hk08394@st.habib.edu.pk>
2023-11-30 11:10:27 +0500

    Messed with features, and made grass dynamic

--- include/features/feature.h
+++ include/features/feature.h
@@ -4,12 +4,15 @@
 class Feature : public Terrain { //e.g. grass, building etc.
 public:
   Feature(const int pos_x, const int pos_y, const int spritesheet_pos_x,
-          const int spritesheet_pos_y);
+          const int spritesheet_pos_y); //by default create grass
   virtual ~Feature();
-  bool walkable = false;
+  virtual void step();
+
+
 
 protected:
-	bool natural;
+  bool walkable = false;
+  std::string type = "grass";
 };
 
 
--- src/feature/grass.cpp
+++ src/feature/grass.cpp
@@ -1,6 +1,36 @@
 #include "../../include/features/grass.h"
 
-Grass::Grass(const int pos_x, const int pos_y, const int spritesheet_pos_x,
+Grass::Grass(const int pos_x, const int pos_y, const int food_level, const int food_capacity, const int spritesheet_pos_x,
 			 const int spritesheet_pos_y)
-	: Feature(pos_x, pos_y, spritesheet_pos_x, spritesheet_pos_y){};
+	: Feature(pos_x, pos_y, spritesheet_pos_x, spritesheet_pos_y), food_level(food_level), food_capacity(food_capacity) {};
 Grass::~Grass() = default;
+
+
+void Grass::grow_back() {
+  if (food_level + 1 < food_capacity) food_level++;
+}
+
+
+void Grass::consume() {
+  if (food_level - 1 > 0) food_level--;
+}
+
+void Grass::update_sprite() {
+  if (food_level == 0) { //barren
+    spritesheet_pos_x = 23;
+    spritesheet_pos_y = 12;
+  }
+
+  else if (food_level < 3) { //seedling
+    spritesheet_pos_x = 23;
+    spritesheet_pos_y = 13;//13 or 12
+  }
+  else if (food_level < 5) { //bud
+    spritesheet_pos_x = 24;
+    spritesheet_pos_y = 13;
+  }
+  else if (food_level < 10) { //forest
+    spritesheet_pos_x = 25;
+    spritesheet_pos_y = 13;
+  }
+}
