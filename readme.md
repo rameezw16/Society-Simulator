74df96854ceb90156cd0260629baabca27d2bb47
Hamad <hk08394@st.habib.edu.pk>
2023-12-17 15:16:30 +0500

    Pretty much done with this now

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ The UML file is available as uml.png. Note that only the signature of the method
 
 The UML diagram was generated using the https://www.plantuml.com/ web server. The code was available at uml-code.txt.
 
-This is a little explanation of our project and the code. Our project is a creative, procedurally generated, approach to the classic sheep-grass simulation, as well as an extension with terrain and seasons. 
+This is a little explanation of our project and the code. Our project is a creative, procedurally generated, approach to the classic sheep-grass simulation extended with randomly generated terrain and season simulation. 
 
 Maximum vegetation levels, dirt, water bodies, people, are all completely randomly generated based on the seed. People have personalities (corresponding to the Big 5 traits in psychology) which are also randomly generated. Seasons come and go based on a set timer, and "seasons" within human population are observed. The cycle observed is as follows. 
 
@@ -25,11 +25,24 @@ Comments are given in the .h files of the major features.
 - The program accepts a seed. Observe how the terrain is completely different at different values, and the placement of the people too. 
 - Terminate the program by sending SIGINT to the terminal, or by closing the window
 
-Example command:
-./main 9 (will generate a random world, seed it with the value 9, and then add people)
-./main (will generate a random world, and use the default seed of 1985, and then add people)
+* How to use the gnuplot functionality
+- You might have to add https://github.com/dstahlke/gnuplot-iostream as a submodule, and fetch it
+- Install gnuplot
+- It should work out of the box
+- You have to specify a seed, and then 0 or 1 for gnuplot functionality
 
-We had a lot of fun working on the project, and really learned a lot. About teamwork, about agent based modelling (which I used in a project for a different class for economic simulations, which I would love to show you), and about programming and computers. Many days were spent in GDB trying to track down some obscure, cryptic error. 
+
+Example commands:
+
+./main 9 (seed the world with a value of 9, no gnuplot)
+
+./main (seed the world with a default seed of 1985, no gnuplot)
+
+./main 9 1 (seed the world with a default seed of 9, with gnuplot)
+
+
+We had a lot of fun working on the project, and really learned a lot. About teamwork, about agent based modelling (which I used in a project for a different class for economic simulations, which I would love to show you), and about programming and computers. Many days were spent in GDB trying to track down some obscure, cryptic error, but it was worth it. All code was created using open source linux tools, which taught us a lot. 
+
 
 Unfortunately due to other projects and deadlines, the code is a little messy at places, but is fully functional and safe. We hope you'll understand.
 
--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,14 @@
 #include <random>
 
 #define GNUPLOT_DEPRECATE_WARN
-// #include "../gnuplot-iostream/gnuplot-iostream.h"
+#include "../gnuplot-iostream/gnuplot-iostream.h"
 
 const int FPS = 60;
 const int frame_delay = 1000 / FPS;
 
 int main(int argc, char **argv) { // takes in seed as cli argument
   unsigned int seed = (argc - 1) ? std::stoi(argv[1]) : 1985;
+  bool plot = (argc - 2) ? std::stoi(argv[2]) : 0;
 
   char const *ss_feature = "../resources/Bountiful-Bits-10x10-v-3.1/Colored/"
                            "no-background/Full-no-bg.png";
@@ -33,49 +34,31 @@ int main(int argc, char **argv) { // takes in seed as cli argument
   static Uint32 next_time;
 
   Game game{seed};
-  // Grid<pointer_terrain> grid;
 
-  // Earth_builder earth;
-  // unique_ptr<Grid<Feature>> features;
-  // unique_ptr<Grid<Terrain>> terrain;
-  // earth.general_generation();
-  // this is in "game" now
-  // features = std::move(earth.get_features());
-  // terrain = std::move(earth.get_terrain());
+  //  Graph Manager
+  Gnuplot GNUpipe;
+  bool success = false;
+  if (plot) {
+    FILE *data_txt = NULL;
+    data_txt = fopen("../logs/data.txt", "w");
+    fprintf(data_txt, "0 0\n");
+    fclose(data_txt);
+    FILE *GNUpipe_file = popen("${GNUPLOT_EXECUTABLE}", "w");
 
-  // for (int i = 0; i < SIZE; ++i) {
-  //   for (int j = 0; j < SIZE; ++j) {
-  //     auto f = std::move(features->get(i, j));
-  //     if (f)
-  //       drawer->draw_sprite(f, i, j);
-  //     auto t = std::move(terrain->get(i, j));
-  //     if (t)
-  //       drawer->draw_sprite(t, i, j);
-  //   };
-  //   std::cout << "\n";
-  // }
+    if (GNUpipe_file == nullptr) {
+      printf("??\n");
+      std::cerr
+          << "Error opening Gnuplot pipe. Make sure you have gnuplot installed "
+             "and have added it as a submodule as in the readme.md"
+          << std::endl;
+    }
+    success = true;
 
-  // drawer->draw_sprite(features->get(1, 1), 1, 1);
-
-  // Graph Manager
-  // Gnuplot GNUpipe;
-  // FILE* data_txt = NULL;
-  // data_txt = fopen("../logs/data.txt", "w");
-  // fprintf(data_txt, "0 0\n");
-  // fclose(data_txt);
-  // FILE* GNUpipe = popen("${GNUPLOT_EXECUTABLE}", "w");
-
-  // if (GNUpipe == nullptr)
-  // {
-  //   printf("??\n");
-  //   // std::cerr << "Error opening Gnuplot pipe" << std::endl;
-  //   // exit(EXIT_FAILURE);
-  // }
-
-  // GNUpipe << "set term qt persist\n";
-  // GNUpipe << "set title \"Population vs Season\" \n";
-  // GNUpipe << "set xlabel \"Season Number\" \n";
-  // GNUpipe << "set ylabel \"Population Size\" \n";
+    GNUpipe << "set term qt persist\n";
+    GNUpipe << "set title \"Population vs Season\" \n";
+    GNUpipe << "set xlabel \"Season Number\" \n";
+    GNUpipe << "set ylabel \"Population Size\" \n";
+  }
 
   Uint32 frame_start;
   Uint32 frame_time;
@@ -84,8 +67,10 @@ int main(int argc, char **argv) { // takes in seed as cli argument
   const int max_count = 3;
 
   while (true) {
-    // GNUpipe << "plot '../logs/data.txt' using 1:2 with lines lw 8 title
-    // 'Population vs Season'\n";
+    if (plot && success) {
+      GNUpipe << "plot '../logs/data.txt' using 1:2 with lines lw 8 title "
+                 "'Population vs Season'\n";
+    }
 
     SDL_Event e;
     while (SDL_PollEvent(&e) != 0) {
